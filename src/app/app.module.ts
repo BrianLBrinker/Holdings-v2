92a9625c58ebf6442f50d65d4479cd15a009b7ec
Brian L Brinker <33704766+BrianLBrinker@users.noreply.github.com>
2023-10-06 15:01:15 -0500

    Tables rendering

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
 import { MatTabsModule } from '@angular/material/tabs';
-import { MatTableModule } from '@angular/material/table'  
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatSortModule } from "@angular/material/sort";
 import { HoldingsListComponent } from './holdings-list/holdings-list.component';
 import { DataTableComponent } from './data-table/data-table.component';
 
@@ -19,8 +23,12 @@ import { DataTableComponent } from './data-table/data-table.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     BrowserModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatProgressSpinnerModule, 
+    MatSortModule,
+    MatTableModule,
     MatTabsModule,
-    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/data-table/data-table.component.ts
+++ src/app/data-table/data-table.component.ts
@@ -4,7 +4,7 @@ import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
 import {MatSort, Sort, MatSortModule} from '@angular/material/sort';
 import {MatTableDataSource, MatTableModule} from '@angular/material/table';
 
-import { Holding } from 'src/app/Holding';
+import { Holding } from '../Holding';
 
 /**
  * @title Table with pagination
@@ -29,27 +29,27 @@ export class DataTableComponent implements AfterViewInit {
   ];
   dataSource = new MatTableDataSource<Holding>(this.holdings);
 
-  // constructor(private _liveAnnouncer: LiveAnnouncer) {}
+  constructor(private _liveAnnouncer: LiveAnnouncer) {}
 
   @ViewChild(MatPaginator) paginator: any = MatPaginator;
-  // @ViewChild(MatSort) sort: any = MatSort;
+  @ViewChild(MatSort) sort: any = MatSort;
 
   ngAfterViewInit() {
+    this.dataSource = new MatTableDataSource<Holding>(this.holdings);
     this.dataSource.paginator = this.paginator;
-    // this.dataSource.sort = this.sort;
-    console.info('holdings', this.holdings);
+    this.dataSource.sort = this.sort;
   }
 
   /** Announce the change in sort state for assistive technology. */
-  // announceSortChange(sortState: Sort) {
-  //   // This example uses English messages. If your application supports
-  //   // multiple language, you would internationalize these strings.
-  //   // Furthermore, you can customize the message to add additional
-  //   // details about the values being sorted.
-  //   if (sortState.direction) {
-  //     this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
-  //   } else {
-  //     this._liveAnnouncer.announce('Sorting cleared');
-  //   }
-  // }
+  announceSortChange(sortState: Sort) {
+    // This example uses English messages. If your application supports
+    // multiple language, you would internationalize these strings.
+    // Furthermore, you can customize the message to add additional
+    // details about the values being sorted.
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
+  }
 }
