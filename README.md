fb7e1f22b7e3ac3644b460e1fc49c1a27e752a00
Brian L Brinker <33704766+BrianLBrinker@users.noreply.github.com>
2023-10-07 15:18:55 -0500

    Removed unnecessary service and refactoring

--- README.md
+++ README.md
@@ -10,9 +10,13 @@ Additional features:
 - There is a no-results message for filtering
 - The UI is keyboard accessible via tab/shift-tab and arrow keys
 
-The project uses two main components: `holdings-list`, which is the main view and contains the Material tabbed container, and `data-table`, which houses the Material tables.  There is also a service, `holdings`, which gets the local `json`.
+The project uses two main components: `holdings-list`, which is the main view and contains the Material tabbed container, and `data-table`, which houses the Material tables.
 
 Although not used, the Angular http-interceptor pattern could have been used to enforce the desired delay simulation; the `RxJS` delay operator was used instead for brevity and simplicity.
+
+Normally, we would add internationalization, however that is not added here.
+
+Normally, such functionality as this would be guarded by user authenication; no attempt was made to implement that here.
 
 Angular Material components are used: `mat-table` tables for each view, including the `mat-paginator`, and the tabbed container to conditionally display either of the two results set tables.  Additional Material components are used for a loading indicator, and a text form/input for filtering the table contents.
 
@@ -22,6 +26,10 @@ This project was generated with [Angular CLI](https://github.com/angular/angular
 
 ## Development server
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
+Clone to download. Then run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files. Angular 16 will be required.
+
+## Standalone
+
+
 
 
--- src/app/holdings-list/holdings-list.component.ts
+++ src/app/holdings-list/holdings-list.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, Observer, of, switchMap } from 'rxjs';
+import { delay, Observable, Observer, of, switchMap } from 'rxjs';
 
-import { HoldingsService } from '../holdings.service';
 import { Holding, RawHolding } from '../Holding';
 
 interface Tab {
@@ -16,56 +15,69 @@ interface Tab {
 })
 export class HoldingsListComponent implements OnInit {
   asyncTabs: Observable<Tab[]>;
-  rencentHoldings: Holding[] = [];
+  recentHoldings: Holding[] = [];
   archivedHoldings: Holding[] = [];
+  dataUrl = '../assets/holdings.json';
 
-  constructor(
-    private holdingsService: HoldingsService
-  ) {
+  constructor() {
     this.asyncTabs = new Observable((observer: Observer<Tab[]>) => {
       setTimeout(() => {
         observer.next([
           {label: 'Most recent', content: 'Most recent holdings'},
           {label: 'Archived', content: 'Archived holdings'}
         ]);
-      }, 1500 + 1000 * Math.random());
+      }, 1500 + 1000 * Math.random()); // Simulate async loading of tab info
     });
   }
 
   ngOnInit(): void {
-    const dataUrl = '../assets/holdings.json';
-    this.getHoldings(dataUrl);
+    this.getHoldings(this.dataUrl);
   }
 
   getHoldings(dataUrl: string): void {
-    const rawHoldings: Observable<RawHolding[]> = this.holdingsService.getLocalFile(dataUrl);
+    /*
+    ** GET the raw holdings data and convert some fields to numeric types so that the numbers will sort properly:
+    */
+    const rawHoldings: Observable<RawHolding[] | unknown> = new Observable(observer => {  
+      fetch(dataUrl)
+        .then(response => response.json())  
+        .then(rawHoldings => {  
+          observer.next(rawHoldings);  
+          observer.complete();  
+        })
+        .catch(err => observer.error(err))  
+    })
+    .pipe(delay(2000 * Math.random()));
     const holdings: Observable<Holding[]> = rawHoldings.pipe(
-      switchMap(rawHoldingArray => {
-          const holdings = rawHoldingArray.map(
-            rawHolding => {
-              return {
-                AccountName: rawHolding.AccountName,
-                SecurityName: rawHolding.SecurityName,
-                SecurityCUSIP: rawHolding.SecurityCUSIP,
-                Quantity: parseFloat(rawHolding.Quantity),
-                FaceValue: parseFloat(rawHolding.FaceValue),
-                MarketValue: parseFloat(rawHolding.MarketValue),
-                UnrealizedGainLoss: parseFloat(rawHolding.UnrealizedGainLoss),
-                EffectiveDate: rawHolding.EffectiveDate
-              }
+      switchMap((rawHoldingArray: any) => {
+        const holdings: Holding[] = rawHoldingArray.map(
+          (rawHolding: RawHolding) => {
+            return {
+              AccountName: rawHolding.AccountName,
+              SecurityName: rawHolding.SecurityName,
+              SecurityCUSIP: rawHolding.SecurityCUSIP,
+              Quantity: parseFloat(rawHolding.Quantity),
+              FaceValue: parseFloat(rawHolding.FaceValue),
+              MarketValue: parseFloat(rawHolding.MarketValue),
+              UnrealizedGainLoss: parseFloat(rawHolding.UnrealizedGainLoss),
+              EffectiveDate: rawHolding.EffectiveDate
             }
-          );
-          return of(holdings);
+          }
+        );
+        return of(holdings);
       })
     );
 
-    holdings.subscribe(holdings => {
-      const mostRecentDate = holdings
+    /*
+    ** Get most recent date and build most-recent and archive lists:
+    */
+    holdings.subscribe(holdingsArray => {
+      const mostRecentDate = holdingsArray
         .map(holding => new Date(holding.EffectiveDate))
         .reduce((mostRecentDate, currentDate) => currentDate > mostRecentDate ? currentDate : mostRecentDate);
 
-      this.rencentHoldings = holdings.filter(holding => new Date(holding.EffectiveDate) >= mostRecentDate);
-      this.archivedHoldings = holdings.filter(holding => new Date(holding.EffectiveDate) < mostRecentDate);
+      this.recentHoldings = holdingsArray.filter(holding => new Date(holding.EffectiveDate) >= mostRecentDate);
+      this.archivedHoldings = holdingsArray.filter(holding => new Date(holding.EffectiveDate) < mostRecentDate);
     });
   }
 }
